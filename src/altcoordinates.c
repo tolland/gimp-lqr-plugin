cf1acd0264d30bbaf2e3af4259d755f2a36843b0
Tom Hodder <tom@limepepper.co.uk>
2026-02-25 11:51:34 +0000

    re-add LICENSE header removed for clion code collapse problem

--- src/altcoordinates.c
+++ src/altcoordinates.c
@@ -1,3 +1,24 @@
+/* GIMP LiquidRescale Plug-in
+ * Copyright (C) 2007-2010 Carlo Baldassi (the "Author") <carlobaldassi@gmail.com>.
+ * All Rights Reserved.
+ *
+ * The code in this file is taken from gimpwidgets.c
+ * Copyright (C) 2000 Michael Natterer <mitch@gimp.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the Licence, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org.licences/>.
+ */
+
 #include <libgimp/gimp.h>
 #include <libgimp/gimpui.h>
 
