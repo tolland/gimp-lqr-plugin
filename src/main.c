6024e20faab5c375886bf024fc883f2acb766b1c
Tom Hodder <tom@limepepper.co.uk>
2026-02-26 19:26:27 +0000

    remove autoformat changes

--- src/main.c
+++ src/main.c
@@ -39,29 +39,29 @@
 #include "defaults.h"
 
 /* Local function prototypes */
-static gint32 layer_from_name(gint32 image_ID,
-                              gchar *name);
-static void set_aux_layer_name(GimpLayer *layer,
-                               gboolean status,
-                               gchar *name);
-static void save_vals(void);
-static void retrieve_vals(void);
-static void retrieve_vals_use_aux_layers_names(gint32 image_ID);
-static void noninteractive_read_vals(GimpProcedureConfig *config,
-                                     GimpImage *image);
-static void install_custom_signals(void);
-static void cancel_work_on_aux_layer(void);
-static GList *lqr_query_procedures(GimpPlugIn *plug_in);
-static GimpProcedure *lqr_create_procedure(GimpPlugIn *plug_in,
-                                           const gchar *name);
-static GimpValueArray *lqr_run(GimpProcedure *procedure,
-                               GimpRunMode run_mode,
-                               GimpImage *image,
-                               GimpDrawable **drawables,
-                               GimpProcedureConfig *config,
-                               gpointer run_data);
+static gint32           layer_from_name                    (gint32 image_ID,
+                                                            gchar *name);
+static void             set_aux_layer_name                 (GimpLayer *layer,
+                                                            gboolean status,
+                                                            gchar *name);
+static void             save_vals                          (void);
+static void             retrieve_vals                      (void);
+static void             retrieve_vals_use_aux_layers_names (gint32 image_ID);
+static void             noninteractive_read_vals           (GimpProcedureConfig *config,
+                                                            GimpImage *image);
+static void             install_custom_signals             (void);
+static void             cancel_work_on_aux_layer           (void);
+static GList           *lqr_query_procedures               (GimpPlugIn *plug_in);
+static GimpProcedure   *lqr_create_procedure               (GimpPlugIn *plug_in,
+                                                            const gchar *name);
+static GimpValueArray  *lqr_run                            (GimpProcedure *procedure,
+                                                            GimpRunMode run_mode,
+                                                            GimpImage *image,
+                                                            GimpDrawable **drawables,
+                                                            GimpProcedureConfig *config,
+                                                            gpointer run_data);
 #if defined(G_OS_WIN32)
-static gchar *get_gimp_share_directory_on_windows(void);
+static gchar           *get_gimp_share_directory_on_windows(void);
 #endif
 
 /*  Local variables  */
